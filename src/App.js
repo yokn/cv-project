389caa1512941fc2ebba69a64fc500f4471a5fcd
yokn <yokn@protonmail.com>
2021-05-26 02:23:45 +0300

    implement GenericForm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import GenericForm from "./components/GenericForm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="App">
+        <GenericForm
+          formElements={[
+            {
+              label: {
+                for: "testlabelfor",
+                text: "testlabeltext",
+              },
+              input: {
+                value: "testinputvalue",
+                type: "testinputtype",
+                id: "testinputid",
+              },
+            },
+          ]}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/GenericForm.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+class GenericForm extends Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  handleChange = (e) => {
+    this.setState({});
+  };
+
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({});
+  };
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onSubmitTask}>
+          {this.props.formElements.map((formElement) => {
+            return (
+              <div>
+                <label htmlFor={formElement.label.for}>
+                  {formElement.label.text}
+                </label>
+                <input
+                  onChange={this.handleChange}
+                  value={formElement.input.value}
+                  type={formElement.input.type}
+                  id={formElement.input.id}
+                ></input>
+              </div>
+            );
+          })}
+          <button type="submit">Save</button>
+        </form>
+      </div>
+    );
+  }
+}
+export default GenericForm;
